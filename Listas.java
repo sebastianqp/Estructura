18c47b1096f2b609eed3a31b004aee3652d0c86b
Vannessa Ayala <132626714+VannessaAyala@users.noreply.github.com>
2023-05-20 22:15:14 -0500

    Creación código xd

--- Listas.java
+++ Listas.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class SumaLista {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numeros = new ArrayList<>();
+
+        System.out.print("Ingrese la cantidad de números: ");
+        int cantidadNumeros = scanner.nextInt();
+
+        for (int i = 0; i < cantidadNumeros; i++) {
+            System.out.print("Ingrese un número: ");
+            int numero = scanner.nextInt();
+            numeros.add(numero);
+        }
+
+        int suma = 0;
+        for (int numero : numeros) {
+            suma += numero;
+        }
+
+        System.out.println("La suma de los números es: " + suma);
+    }
+}
